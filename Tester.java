581fb818bbf1278e608fb373c7d529b53a3f1049
rsbarhey <rsbarhey@zone07-wd.cs.ucalgary.ca>
2015-10-07 16:41:14 -0600

    The tester1 file had to replace tester

--- Tester.java
+++ Tester.java
@@ -3,24 +3,34 @@
  * A simple test driver
  * 
  * @author 	Majid Ghaderi
- * @version	1.0, Sep 22, 2015
+ * @version	3.0, Oct 2, 2015
  *
  */
 public class Tester {
 	
 	public static void main(String[] args) {
-		UrlCache cache = new UrlCache();
 
-		String[] url = {"people.ucalgary.ca/~mghaderi/majid.jpg",
-						"people.ucalgary.ca/~mghaderi/cpsc441/index.html",
-						"www.oracle.come:80/2015/08/mobile-web-app-state/Overview.pdf"};
+		// include whatever URL you like
+		// these are just some samples
+		String[] url = {"people.ucalgary.ca/~mghaderi/test/test.html",
+						"people.ucalgary.ca/~mghaderi/test/uc.gif",
+						"people.ucalgary.ca:80/~mghaderi/test/a1.pdf"};
 		
-		for (int i = 0; i < url.lrngth; i++)
+		// this is a very basic tester
+		// the TAs will use more a comprehensive set of tests
+		try {
+			UrlCache cache = new UrlCache();
+			
+			for (int i = 0; i < url.length; i++)
+				cache.getObject(url[i]);
+			
+			System.out.println("Last-Modified for " + url[0] + " is: " + cache.getLastModified(url[0]));
 			cache.getObject(url[0]);
-		
-		System.out.println("Last-Modified for " + url[0] + " is:" + cache.getLastModified(url[0]));
-		cache.getObject(url[0]);
-		System.out.println("Last-Modified for " + url[0] + " is:" + cache.getLastModified(url[0]));
+			System.out.println("Last-Modified for " + url[0] + " is: " + cache.getLastModified(url[0]));
+		}
+		catch (UrlCacheException e) {
+			System.out.println("There was a problem: " + e.getMessage());
+		}
 	}
 	
 }
